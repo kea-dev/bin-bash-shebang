b9b751a79736ec0b302a21fcff537d555f688e93
Lars Kruse <lakruzz@inc-inc.dk>
2023-02-04 15:35:45 +0000

    Added a script the creates sample files

---
+++ setup-samples.sh
@@ -0,0 +1,26 @@
+mkdir samples
+mkdir samples/animals
+mkdir samples/animals/countries
+mkdir samples/animals/countries/movies
+
+ANIMALS=('Apes' 'Elephants' 'Elephant babies' 'Monkies' 'apes')
+COUNTRIES=('Denmark' 'Sweden' 'USA' 'France' 'Italy' '.apes' 'North Korea')
+MOVIES=('Batman' 'Sex And The City' 'JFK' 'Poltergeist' 'Germany')
+
+cd samples/animals 
+for id in ${ANIMALS[@]}
+do
+echo $id >> $id.txt
+done
+
+cd countries
+for id in ${COUNTRIES[@]}
+do
+echo $id >> $id.txt
+done
+
+cd movies
+for id in ${MOVIES[@]}
+do
+echo $id >> $id.txt
+done
