a78b7a900af3543523695eafaf1ffb04ada87cfd
Lars Kruse <lakruzz@inc-inc.dk>
2023-02-04 15:51:09 +0000

    Added a script that fixes the samples

---
+++ fix-samples.sh
@@ -0,0 +1,8 @@
+ cd samples/
+ mv animals/countries/ .
+ mv countries/movies/ .
+ mv animals/Monkies.txt animals/Monkeys.txt 
+ echo Monkeys > animals/Monkeys.txt 
+ mv movies/Germany.txt countries/
+ mv countries/.apes.txt animals/
+ cd -
