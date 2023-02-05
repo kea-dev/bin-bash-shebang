c15aa82b5886981be6d326d94367eee133dc9180
Lars Kruse <lakruzz@inc-inc.dk>
2023-02-05 21:59:13 +0100

    Autograde

--- .github/classroom/test1.sh
+++ .github/classroom/test1.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-pwd
-echo OK
+# Make sure that the sample files are not comitted to the repo
+if [ -d ./samples ]
+then
+  echo "The directory 'samples' is checked in - that's a mistake"
+  exit 1
+else
+  echo "Fine. No 'samples' is checked in to the repo!"
+fi
--- .github/classroom/test2.sh
+++ .github/classroom/test2.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-pwd
-echo OK
+# Test if the setup-samples.sh exist and does what it should
+if [ -x setup-samples.sh ]
+then
+ ./setup-samples.sh
+
+  echo "Looking for './samples/animals/countries/.apes.txt'"
+  cat ./samples/animals/countries/.apes.txt || exit 255
+
+  echo "Looking for './samples/animals/Elephant babies.txt'"
+  cat "./samples/animals/Elephant babies.txt" || exit 255
+
+  exit 0
+else
+  echo "There is no executable file named 'setup-samples.sh'"
+  exit 255
+fi
